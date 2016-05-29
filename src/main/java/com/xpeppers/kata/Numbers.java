bf74bddbc6aefc2f955a87de9f8592dd18845610
Pietro Di Bello <pierodibello@gmail.com>
2016-05-29 21:21:04 +0200

    gaps for the set (4,1) is ["2-3"]

--- src/main/java/com/xpeppers/kata/Numbers.java
+++ src/main/java/com/xpeppers/kata/Numbers.java
@@ -1,6 +1,8 @@
 package com.xpeppers.kata;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -13,7 +15,26 @@ public class Numbers {
     }
 
     public String[] gaps() {
-        Optional<Integer> max = numbers.stream().max(Integer::max);
+        Integer[] opposites = oppositeOf(numbers);
+        if (opposites.length == 1) {
+            return new String[] { opposites[0] + "-" + opposites[0] };
+        }
+
+        List<String> gaps = new ArrayList<String>();
+        for (int i = 0, j = 1; j < opposites.length; i++, j++) {
+            Integer lowerBound = opposites[i];
+            Integer upperBound = opposites[i];
+            if (lowerBound + 1 == opposites[j]) {
+                upperBound = opposites[j];
+            }
+            gaps.add(lowerBound + "-" + upperBound);
+        }
+
+        return gaps.toArray(new String[0]);
+    }
+
+    private Integer[] oppositeOf(Set<Integer> numbers) {
+        Optional<Integer> max = maxValueOf(numbers);
 
         Set<Integer> opposites = new HashSet<Integer>();
         for (int number = 1; number <= max.orElse(0); number++) {
@@ -22,12 +43,11 @@ public class Numbers {
             }
         }
 
-        HashSet<String> gaps = new HashSet<String>();
-        for (Integer number : opposites) {
-            gaps.add(number + "-" + number);
-        }
+        return opposites.toArray(new Integer[0]);
+    }
 
-        return gaps.toArray(new String[0]);
+    private Optional<Integer> maxValueOf(Set<Integer> collection) {
+        return collection.stream().max(Integer::compare);
     }
 
     public void add(int number) {
--- src/test/java/com/xpeppers/kata/NumbersTest.java
+++ src/test/java/com/xpeppers/kata/NumbersTest.java
@@ -2,13 +2,20 @@ package com.xpeppers.kata;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class NumbersTest {
 
+    private Numbers numbers;
+
+    @Before
+    public void setup() {
+        numbers = new Numbers();
+    }
+
     @Test
     public void no_gaps_for_an_empty_set_of_numbers() {
-        Numbers numbers = new Numbers();
         assertThat(numbers.gaps()).isEmpty();
     }
 
@@ -22,7 +29,6 @@ public class NumbersTest {
 
     @Test
     public void gaps_1_1_for_a_set_with_just_number_2() throws Exception {
-        Numbers numbers = new Numbers();
         numbers.add(2);
 
         assertThat(numbers.gaps()).isEqualTo(new String[] { "1-1" });
@@ -30,11 +36,18 @@ public class NumbersTest {
 
     @Test
     public void no_gaps_for_a_set_with_numbers_1_and_2() throws Exception {
-        Numbers numbers = new Numbers();
         numbers.add(1);
         numbers.add(2);
 
         assertThat(numbers.gaps()).isEmpty();
+    }
+
+    @Test
+    public void gaps_2_3_for_set_with_numbers_4_and_1() throws Exception {
+        numbers.add(4);
+        numbers.add(1);
+
+        assertThat(numbers.gaps()).isEqualTo(new String[] { "2-3" });
     }
 
 }
